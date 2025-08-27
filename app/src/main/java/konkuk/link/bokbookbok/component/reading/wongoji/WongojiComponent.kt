8a869b9e4b76151eb03a2cf5bc2e6c8f9e3d8a9f
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-28 00:54:15 +0900

    [refactor] Fix Code Warnings

--- app/src/main/java/konkuk/link/bokbookbok/component/reading/wongoji/WongojiComponent.kt
+++ app/src/main/java/konkuk/link/bokbookbok/component/reading/wongoji/WongojiComponent.kt
@@ -2,16 +2,13 @@ package konkuk.link.bokbookbok.component.reading.wongoji
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Surface
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -20,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import konkuk.link.bokbookbok.ui.theme.bokBookBokColors
 import konkuk.link.bokbookbok.ui.theme.defaultBokBookBokTypography
@@ -99,7 +95,7 @@ fun WonGoJiBoard(
             WonGoJiRow(text = line)
 
             if (index < processedLines.size - 1) {
-                Divider(
+                HorizontalDivider(
                     thickness = 1.dp,
                     color = bokBookBokColors.second
                 )
@@ -113,7 +109,7 @@ private fun processTextForWonGoJi(text: String): List<String> {
 
     val lines = mutableListOf<String>()
     val words = text.split(' ')
-    var currentLine = StringBuilder()
+    val currentLine = StringBuilder()
 
     words.forEach { word ->
         if (word.length > 8) {
