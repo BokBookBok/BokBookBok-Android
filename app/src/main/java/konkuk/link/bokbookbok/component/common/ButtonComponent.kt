77bbfa06e1fbf4e613be3f10aab4d78e3f8470a8
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-27 17:49:20 +0900

    [docs] Delete unused imports

--- app/src/main/java/konkuk/link/bokbookbok/component/common/ButtonComponent.kt
+++ app/src/main/java/konkuk/link/bokbookbok/component/common/ButtonComponent.kt
@@ -2,7 +2,6 @@ package konkuk.link.bokbookbok.component.common
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
