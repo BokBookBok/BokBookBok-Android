99b3a7adb20b37860165d5afc15e95759613124e
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-28 02:50:26 +0900

    [feat] Add Register ApiService

--- app/src/main/java/konkuk/link/bokbookbok/data/repository/AuthRepository.kt
+++ app/src/main/java/konkuk/link/bokbookbok/data/repository/AuthRepository.kt
@@ -1,11 +1,17 @@
 package konkuk.link.bokbookbok.data.repository
 
-import konkuk.link.bokbookbok.data.model.request.RegisterRequest
-import konkuk.link.bokbookbok.data.model.response.RegisterResponse
+import konkuk.link.bokbookbok.data.model.request.register.RegisterEmailRequest
+import konkuk.link.bokbookbok.data.model.request.register.RegisterNicknameRequest
+import konkuk.link.bokbookbok.data.model.request.register.RegisterRequest
+import konkuk.link.bokbookbok.data.model.response.register.RegisterEmailResponse
+import konkuk.link.bokbookbok.data.model.response.register.RegisterNicknameResponse
+import konkuk.link.bokbookbok.data.model.response.register.RegisterResponse
 import konkuk.link.bokbookbok.data.remote.ApiService
 
 class AuthRepository(
     private val apiService: ApiService,
 ) {
     suspend fun registerUser(request: RegisterRequest): Result<RegisterResponse> = safeApiCall { apiService.register(request) }
+    suspend fun registerEmailUser(request: RegisterEmailRequest): Result<RegisterEmailResponse> = safeApiCall { apiService.registerEmailDuplicate(request) }
+    suspend fun registerNicknameUser(request: RegisterNicknameRequest): Result<RegisterNicknameResponse> = safeApiCall { apiService.registerNicknameDuplicate(request) }
 }
