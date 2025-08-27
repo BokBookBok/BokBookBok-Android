9a592b774355a0e29179d6cd8247d334a767e713
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-28 03:05:26 +0900

    [refactor] Remove Unused imports

--- app/src/main/java/konkuk/link/bokbookbok/component/common/AlertComponent.kt
+++ app/src/main/java/konkuk/link/bokbookbok/component/common/AlertComponent.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import konkuk.link.bokbookbok.ui.theme.bokBookBokColors
 import konkuk.link.bokbookbok.ui.theme.defaultBokBookBokTypography
 
