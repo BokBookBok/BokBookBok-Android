82158ed10364186e38274066bceb8f17d14c6e83
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-08-29 00:16:13 +0900

    [feat] Add Record DTO

---
+++ app/src/main/java/konkuk/link/bokbookbok/data/model/response/record/RecordHomeResponse.kt
@@ -0,0 +1,28 @@
+package konkuk.link.bokbookbok.data.model.response.record
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RecordHomeResponse(
+    val nickname: String,
+    val totalCount: Int,
+    val records: List<Record>,
+)
+
+@Serializable
+data class Record(
+    val bookInfoResponse: BookInfoResponse,
+    val readDays: Int,
+    val startedAt: String,
+    val endedAt: String,
+    val weekLabel: String
+)
+
+@Serializable
+data class BookInfoResponse(
+    val id: Int,
+    val title: String,
+    val author: String,
+    val description: String,
+    val imageUrl: String
+)
